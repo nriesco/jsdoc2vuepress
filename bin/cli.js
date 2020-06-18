8758acfd6a22ac8f9216689c33ed23dccbff82e9
Nico R <riesco@gmail.com>
2020-06-18 14:36:25 -0400

    environment to set partial's path

--- bin/cli.js
+++ bin/cli.js
@@ -9,18 +9,20 @@ let outputFile = 'DOCS.md';
 if (process.argv.length >= 3) files = [process.argv[2]];
 if (process.argv.length >= 4) outputFile = process.argv[3];
 
+let partial = ['node_modules/jsdoc2vuepress/files/header.hbs'];
+if (process.env.DEBUG_LOCAL) partial = ['files/header.hbs'];
+
 const options = { 
   files,
-  partial: ['node_modules/jsdoc2vuepress/files/header.hbs'] // dev environment
-  // partial: ['files/header.hbs'] // local environment
-}
+  partial
+};
 
-const cleanFlowChart = require('./cleanFlowChart')
+const cleanFlowChart = require('./cleanFlowChart');
 
 jsdoc2md
   .render(options)
   .then(output => {
-    output = cleanFlowChart(output)
+    output = cleanFlowChart(output);
     fs.writeFileSync(outputFile, output);
     process.exit(0);
   })
