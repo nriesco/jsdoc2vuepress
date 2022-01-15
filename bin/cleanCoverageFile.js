c1ff28c7e5949bc1cc4afd3783b6942d6437dfb5
Nico R <riesco@gmail.com>
2022-01-15 09:16:20 -0300

    new version
    moving to standardjs and changes in the resulting files

--- bin/cleanCoverageFile.js
+++ bin/cleanCoverageFile.js
@@ -2,12 +2,12 @@
  * get the date in YYYY-MM-DD format
  */
 const getFormattedDate = () => {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  const yyyy = today.getFullYear();
+  const today = new Date()
+  const dd = String(today.getDate()).padStart(2, '0')
+  const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  const yyyy = today.getFullYear()
 
-  return(yyyy + '-' + mm + '-' + dd);
+  return (yyyy + '-' + mm + '-' + dd)
 }
 
 /**
@@ -28,43 +28,43 @@ const getFormattedDate = () => {
  */
 const clean = (data) => {
   // add a space if needed
-  data = data.replace(/^  #([^ ]+)(.*)$/gm, '  ### \$1$2');
-  data = data.replace(/^  -([^ ]+)(.*)$/gm, '  - \$1$2'); // pending
-  data = data.replace(/^  ✓([^ ]+)(.*)$/gm, '  ✓ \$1$2'); // passing
+  data = data.replace(/^  #([^ ]+)(.*)$/gm, '  ### \$1$2')
+  data = data.replace(/^  -([^ ]+)(.*)$/gm, '  - \$1$2') // pending
+  data = data.replace(/^  ✓([^ ]+)(.*)$/gm, '  ✓ \$1$2') // passing
 
   // remove indentation
-  data = data.replace(/^  /gm, '');
+  data = data.replace(/^  /gm, '')
 
   // remove top and bottom frame for text reporter
-  data = data.replace(/^[-]+.*\nFile/gm, '\n## Coverage Results\n\nFile');
-  data = data.replace(/^.*[-]+\n\n/gm, '\n');
+  data = data.replace(/^[-]+.*\nFile/gm, '\n## Coverage Results\n\nFile')
+  data = data.replace(/^.*[-]+\n\n/gm, '\n')
 
   // remove top and bottom frame for text-summary reporter
-  data = data.replace(/^=+.*=+$/gm, '');
+  data = data.replace(/^=+.*=+$/gm, '')
 
   // transform into list
-  let keyWords = ['Statements', 'Branches', 'Functions', 'Lines'];
+  const keyWords = ['Statements', 'Branches', 'Functions', 'Lines']
   keyWords.forEach(keyWord => {
-    const replace = '^' + keyWord;
-    const re = new RegExp(replace, 'gmi');
-    data = data.replace(re, '- ' + keyWord);
+    const replace = '^' + keyWord
+    const re = new RegExp(replace, 'gmi')
+    data = data.replace(re, '- ' + keyWord)
   })
 
-  let package = require('../package.json');
+  const packageData = require('../package.json')
 
   // add title and other formatted info
-  data = `# Report
+  data = `# Coverage Report
 
-::: tip ${package.name}@${package.version}
+::: tip ${packageData.name}@${packageData.version}
 
 _Generated: ${getFormattedDate()}_
 :::
 
 ## Tests
 
-` + data;
+` + data
 
-  return data;
+  return data
 }
 
-module.exports = clean;
+module.exports = clean
--- bin/cli.js
+++ bin/cli.js
@@ -1,40 +1,41 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const jsdoc2md = require('jsdoc-to-markdown');
-
-
-// default value
-let files = ['src/*.{js,ts}'];
-if (process.argv.length >= 3) files = [process.argv[2]];
+const fs = require('fs')
+const jsdoc2md = require('jsdoc-to-markdown')
 
 // default value
-let prefix = 'dist/';
-if (process.env.DEBUG_LOCAL) prefix = 'files/'; // add prefix for testing purposes
+let files = ['src/*.{js,ts}']
+if (process.argv.length >= 3) files = [process.argv[2]]
 
 // default value
-if (process.argv.length >= 4) prefix = process.argv[3];
-let outputFile = prefix + 'DOCS.md';
+let prefix = 'dist/'
+if (process.env.DEBUG_LOCAL) prefix = 'files/' // add prefix for testing purposes
 
+// default value
+if (process.argv.length >= 4) prefix = process.argv[3]
+const outputFile = prefix + 'DOCS.md'
 
-let partial = ['node_modules/jsdoc2vuepress/files/header.hbs'];
-if (process.env.DEBUG_LOCAL) partial = ['files/header.hbs'];
+let partial = ['node_modules/jsdoc2vuepress/files/header.hbs']
+if (process.env.DEBUG_LOCAL) partial = ['files/header.hbs']
 
-const options = { 
+const options = {
   files,
   partial
-};
+}
 
-const cleanFlowChart = require('./cleanFlowChart');
+const title = '# Docs\n\n'
+
+const cleanFlowChart = require('./cleanFlowChart')
 
 jsdoc2md
   .render(options)
   .then(output => {
-    output = cleanFlowChart(output);
-    fs.writeFileSync(outputFile, output);
-    process.exit(0);
+    output = cleanFlowChart(output)
+    output = title + output
+    fs.writeFileSync(outputFile, output)
+    process.exit(0)
   })
   .catch(err => {
-    console.log('>> some error', err);
+    console.log('>> some error', err)
   })
 
 /*
@@ -43,8 +44,9 @@ jsdoc2md
  * read from COVERAGE_RAW.md
  * write to COVERAGE.md
  */
-const coverageFile = fs.readFileSync(prefix + 'COVERAGE_RAW.md');
-const cleanCoverageFile = require('./cleanCoverageFile');
-const cleanData = cleanCoverageFile(coverageFile.toString()); // remember to use `.toString()`
+const coverageFile = fs.readFileSync(prefix + 'COVERAGE_RAW.md')
+const cleanCoverageFile = require('./cleanCoverageFile')
+const cleanData = cleanCoverageFile(coverageFile.toString()) // remember to use `.toString()`
 // write the file
 fs.writeFileSync(prefix + 'COVERAGE.md', cleanData)
+fs.unlinkSync(prefix + 'COVERAGE_RAW.md') // delete old file
